c06a61c5128d315506d0af17ac2384161f480dd2
khattori <kaha8t@gmail.com>
2016-11-14 13:35:39 +0900

    プロトタイプ複数人ver

--- jsonEdit.php
+++ jsonEdit.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
 
 const JSON_PATH = "member_data.json";
 
@@ -6,11 +7,19 @@ $id = $_POST["id"];
 
 $json = json_decode(file_get_contents(JSON_PATH));
 
-if( $json->member->$id->status == "home") {
+if( isset($_POST['submit_x']) & isset($_POST['submit_y']) ) {
+	if( $json->member->$id->status == "campus" ) {
+		$json->member->$id->status = "lab";
+	} else {
+		$json->member->$id->status = "campus";
+	}
+} else if ( $json->member->$id->status == "home" ) {
 	$json->member->$id->status = "lab";
 } else {
 	$json->member->$id->status = "home";
 }
+
+$json->member->$id->modified_date = date("Y/m/d H:i:s");
 
 file_put_contents(JSON_PATH, json_encode($json)); 
 
