892e91b7851aa23432624795b1d5611836724e3c
khattori <kaha8t@gmail.com>
2016-11-20 14:51:26 +0900

    optimized index.css. close #22

--- index.php
+++ index.php
@@ -1,64 +1,26 @@
-<?php
-date_default_timezone_set('Asia/Tokyo');
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <div id="header">
+	  <span><f1>CSL Members</f1></span>
+	</div>
+	
+	<table id="member" class="member-table">
+	<?php
+	  include "member_box.php";
+	  getAllMembersTable($members, 4);
+	?>
+	</table>
+	
+	<br><br>
+	<div id="footer">
+	  <form method="post" action="member_manager.php">
+	    <input type="submit" value="メンバー管理画面"> 
+	  </form>
+	</div>
 
-include "model.php";
-
-$members = array();
-
-$model = new Model();
-$ids = $model->getMemberList();
-foreach($ids as $id) {
-	$m = $model->getMemberDetail($id);
-	$members[] = $m;
-}
-
-// 3日以内に更新があるかどうかのチェック
-function checkUpdate($member) {
-	$timestamp = strtotime("-3 day");
-	$lastUpdateTime = strtotime($member->modified_date);
-	if( $lastUpdateTime - $timestamp < 0 ) {
-		return false;
-	}
-	return true;
-}
-
-function getMemberBox($member) {
-	echo "\t\t<td>\n";
-	if (checkUpdate($member)) {
-		echo	"\t\t<div class=\"member-holder\">\n";
-	} else {
-		echo "\t\t<div class=\"member-holder non-update\">\n";
-	}
-    echo	"\t\t\t<div class=\"member-view member-choice-state-".$member->status."-color\">\n";
-	echo "\t\t\t\t<div class=\"member-state\">\n";
-	echo "\t\t\t\t\t\t<span>".$member->name."@".$member->status."</span>\n";
-	echo "\t\t\t\t</div>\n";
-	echo "\t\t\t\t<div class=\"member-field\">\n";
-	echo "\t\t\t\t\t\t<form method=\"post\" action=\"jsonEdit.php\">\n";
-	echo "\t\t\t\t\t\t\t<input type=\"hidden\" name=\"id\" value=\"".$member->id."\" >\n";
-	echo "\t\t\t\t\t\t\t<input type=\"image\" name=\"submit\" width=\"80px\" height=\"40px\" style=\"border:solid 5px #FFFFFF\" src=\"".$member->image."\" >\n";
-	echo "\t\t\t\t\t\t\t<div class=\"member-info\">\n";
-	echo "\t\t\t\t\t\t\t\t<input type=\"submit\" value=\"".$member->comment."\">\n";
-	echo "\t\t\t\t\t\t\t</div>\n";	
-	echo "\t\t\t\t\t\t</form>\n";
-	echo "\t\t\t\t\t</div>\n";
-	echo "\t\t\t\t</div>\n";
-	echo "\t\t\t</div>\n";
-	echo "\t\t</div>\n";
-	echo "\t\t</td>\n";
-}
-
-function getAllMembersTable($members, $border) {
-	echo "<tr>\n";	
-    $count = 0;
-    foreach ($members as $member) {
-    	getMemberBox($member);
-    	$count++; 
-    	
-    	// $border人ごとに段落  			
-    	if ($count % $border == 0) 	echo "\t\t</tr><tr>\n";
-    }    
-    echo "\t\t</tr>";
-}
-
-?>
+  </body>
+</html>
