85ea0638ce2425a459327cab971bec2666ec3650
hamayanhamayan <bz.programmer@gmail.com>
2016-12-04 02:44:57 +0900

    ログイン処理の実装

---
+++ member_login.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once('./login.php');
+$manager = new LoginManager();
+
+$msg = "";
+
+// POST処理が来たときはログイン処理
+if(isset($_POST['id'])) {
+
+	$id = $_POST['id'];
+	$pass = $_POST['pass'];
+
+	$result = $manager->login($id, $pass);
+	if(!$result) {
+		$msg = "ログインに失敗しました";
+	}
+}
+
+if($manager->isLogined()) {
+	$url = './member_manager.php';
+	header("Location: $url");
+}
+
+?>
+
+<html>
+<head>
+<title>ログイン処理</title>
+<style>
+.login-box {
+    border: 2px solid #da4033;
+    border-radius: 4px;
+    padding: 2em;
+    position: relative;
+    width: 300px;
+    margin: 10em auto;
+    text-align: center;
+}
+.login-box::before {
+    background-color: #fff;
+    color: #da4033;
+    content: "LOGIN";
+    font-weight: bold;
+    left: 1em;
+    padding: 0 .5em;
+    position: absolute;
+    top: -1em;
+}
+</style>
+</head>
+<body>
+
+
+<div class="login-box">
+<?php echo $msg . '<br>'; ?>
+<form action="member_login.php", method="post">
+	  ID <input type="text" name="id"><br>
+	PASS <input type="password" name="pass"><br>
+	<input type="submit" value="ログイン">
+</form>
+<a href="index.php">トップへ戻る</a>
+</div>
+
+
+
+</body>
+</html>
