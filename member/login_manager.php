ff854ff4715cd8f82b8408ce289a6c358ff80fec
hamayanhamayan <bz.programmer@gmail.com>
2016-12-14 13:28:23 +0900

    トークンの生成、認証機能を追加

--- member/login_manager.php
+++ member/login_manager.php
@@ -33,13 +33,14 @@ class LoginManager {
 	}
 
 
-	public function getHash($raw) {
+	public function getHash($raw, $salt = "") {
 	    $chars = array_merge(range('a', 'z'), range('A', 'Z'), array('.', '/'));
 
 	    // ソルト作成
-	    $salt = '';
-	    for ($i = 0; $i < 22; $i++) {
-	        $salt .= $chars[mt_rand(0, count($chars) - 1)];
+	    if($salt === "") {
+	    	for ($i = 0; $i < 22; $i++) {
+		        $salt .= $chars[mt_rand(0, count($chars) - 1)];
+		    }
 	    }
 
 	    return crypt($raw, '$2y$' . '4' . '$' . $salt);
@@ -50,5 +51,40 @@ class LoginManager {
 			return true;
 		else
 			return false;
+	}
+
+	// 以下、トークン用メソッド
+
+	/*
+	 * IDとパスワードのトークン取得
+	 * 
+	 * @param string $id : ユーザーID
+	 * @param string $pass : ユーザーPASS
+	 * @return string ログイン成功ならトークン、ログイン失敗なら""を返す
+	 *
+	 */
+	public function getToken($id, $pass) {
+		if($this->login($id, $pass))
+			return $this->getHash($id, HASHED_ID) . "|" . $this->getHash($pass, HASHED_PASS);
+		else
+			return "";
+	}
+
+	/*
+	 * トークンによるログイン処理
+	 * 
+	 * @param string $token : トークン文字列
+	 * @return bool 正しいトークンならtrue, そうでなければfalseを返す
+	 *
+	 */
+	public function loginByToken($token) {
+		$token_array = split("|", $token);
+
+		if(HASHED_ID === $token_array[0] && HASHED_PASS === $token_array[1]) {
+			$_SESSION["logged"] = "logined";
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
